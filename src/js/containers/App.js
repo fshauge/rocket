b0a4999f5fdd1b2e2a5919f503aaf201322aec0f
fshauge <markus.hauge@hotmail.com>
2018-10-11 23:26:55 +0200

    Refactor App + routes

--- src/js/containers/App.js
+++
@@ -1,4 +0,0 @@
-import Navigation from '../rocket/components/Navigation.js';
-import routes from '../routes.js';
-
-export default () => Navigation({ routes });
--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,5 @@
 import * as Rocket from './rocket/index.js';
-import App from './containers/App.js';
+import App from './containers/App/index.js';
 
 Rocket.ready(() => {
   Rocket.render('#root', App);
